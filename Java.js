0025f71d55f3d12d23d26d4f884c017348281e4b
raappel <raappel@hotmail.com>
2017-02-17 14:31:35 -0500

    Add files via upload

--- Java.js
+++ Java.js
@@ -1,13 +1,31 @@
-function main(){
+$(document).ready(function() {
+
 $("#Youlink").hide();
 $('#Youlink').fadeIn(2000);
 
-$('.Stern').hide();
-$('.Button').on('click', function(){
-$(this).next().toggle();
+$('.Button').click(function(){
+	var input = $('input[name=Eingabe]').val();
+
+	switch(input) {
+		case "Hallo" : $('.liste').append('<div>' + input + ' Mushi</div>');
+		break;
+		case "Hoi" : $('.liste').append('<div>' + input + ' Schwuchtle</div>');
+		break;
+		default: $('.liste').append('<div>Gib Hallo oder Hoi ein</div>');
+		break;
+	}
 });
-};
-$(document).ready(main);
+
+$('input').mouseenter(function() {
+	$(this).css('border-color', 'blue');
+});
+
+$('input').mouseleave(function() {
+	$(this).css('border-color', 'black');
+});
+
+
+});
 
 
 
